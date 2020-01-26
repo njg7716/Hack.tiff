550c83b386e338c12c8dfd97281c0c2b61578c44
Logan Murdock <lxm8708@rit.edu>
2020-01-25 21:34:48 -0500

    finddistance2.py is final solution. added cmdline args

--- poolpic/finddistance2.py
+++ poolpic/finddistance2.py
@@ -4,9 +4,18 @@ import json
 from math import sin, cos, sqrt, atan2, radians
 import matplotlib.pyplot as plt
 from matplotlib.legend_handler import HandlerLine2D
+import argparse
 
+# Load arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-d",
+                    "--dataset",
+                    help="Location of dataset",
+                    type=str)
+args = parser.parse_args()
 
-path = "/home/lxm8708/Hacktiff/poolpic/12996748"
+path = args.dataset#"/home/lxm8708/Hacktiff/poolpic/12996748"
+jsonpath = path + '/' + path.split('/')[-1] + '.geojson'
 
 def getDistance(_lat1, _long1, _lat2, _long2):
 
@@ -26,9 +35,7 @@ def getDistance(_lat1, _long1, _lat2, _long2):
     
     distance = R * c
     
-    #print("Result:", distance)
-    #print("Should be:", 278.546, "km")
-    #1km = 3280.84 feet 
+    #convert to feet
     return 3280.84 * distance
 
 def midpoint(_lat1, _long1, _lat2, _long2):
@@ -47,14 +54,11 @@ def dist3D(dist1, z1, z2):
 
 
 
-with open("/home/lxm8708/Hacktiff/poolpic/12996748/12996748.geojson") as f:
-#with open("/local/2020_hackathon/2020_hackathon/12996748/12996748.geojson") as f:
+with open(jsonpath) as f:
     data = json.load(f)
 
-    #Loop through polygons
+    #Loop through polygons and draw every side
     for feature in data['features']:
-        #prints "Polygon"
-        #print(feature['geometry']['type'])
         firsttime = True
         counter = 0
         for point in feature['geometry']['coordinates'][0]:
@@ -92,41 +96,25 @@ with open("/home/lxm8708/Hacktiff/poolpic/12996748/12996748.geojson") as f:
                 x_array.append(pt1Lat)
                 y_array.append(pt1Lon)
 
-                #print('\n\n\n\n')
-                #print(pt1Lat)
-                #print(pt1Lon)
-                #print(midpointx)
-                #print(midpointy)
-                #print(pt2Lat)
-                #print(pt2Lon)
-                #print('\n\n\n\n')
                 firsttime = True
 
             
-            #print("Printing dimensions from point " + str(counter) + " to " + str(counter + 1))
             measurement = getDistance(pt1Lat, pt1Lon, pt2Lat, pt2Lon)
             measurement = dist3D(measurement, pt1Alt, pt2Alt)
             
-            #print("Measurement made: " + str(measurement))
-            #print()
+            
             counter += 1
             temp = plt.plot(x_array, y_array, label=str('%.1f' % measurement) + " ft")
             
-            #plt.legend(handler_map={temp[0]: HandlerLine2D(numpoints=2)})
-            plt.annotate(str('%.1f' % measurement) + " ft",(midpointx,midpointy)) # this is the point to label
-            #print(temp[0])
-        #print("\n\nFinished polygon\n\n")
-        #print(feature['geometry']['coordinates'])
-        #break
+            #adding labels at the midpoints of the lines
+            plt.annotate(str('%.1f' % measurement) + " ft",(midpointx,midpointy)) 
 
-
-        #plt.plot(x_array, y_array)
 plt.axis('off')
 plt.show()
     
 
 
-
+#Provided test code used to display the image used
 ########################################################################################
 # Only care about nadirs for this app
 nadirs, obliques = evtech.load_dataset(path)
